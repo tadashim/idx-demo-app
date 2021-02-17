70c5f4aa0b627c585106eda52f0ced21e0be6c6a
tadashim <ma.tadashi@gmail.com>
2021-02-17 22:28:27 +0900

    create the styles for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import type { Doctype } from '@ceramicnetwork/common'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Divider from '@material-ui/core/Divider'
+import Drawer from '@material-ui/core/Drawer'
+import Hidden from '@material-ui/core/Hidden'
+import IconButton from '@material-ui/core/IconButton'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import Paper from '@material-ui/core/Paper'
+import TextareaAutosize from '@material-ui/core/TextareaAutosize'
+import TextField from '@material-ui/core/TextField'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
+} from '@material-ui/core/styles'
+import DownloadIcon from '@material-ui/icons/CloudDownload'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
+import ErrorIcon from '@material-ui/icons/ErrorOutline'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import MenuIcon from '@material-ui/icons/Menu'
+import NoteIcon from '@material-ui/icons/Note'
+import NoteAddIcon from '@material-ui/icons/NoteAdd'
+import UploadIcon from '@material-ui/icons/CloudUpload'
+import { randomBytes } from '@stablelib/random'
+import React, { useRef, useState } from 'react'
+import { fromString, toString } from 'uint8arrays'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useApp } from './state'
+import type {
+  AuthState,
+  DraftStatus,
+  IndexLoadedNote,
+  State,
+  StoredNote,
+} from './state'
 
-export default App;
+const drawerWidth = 300
+
+const useStyle = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    noteSaveButton: {
+      marginTop: theme.spacing(2),
+    },
+    noteTextarea: {
+      border: 0,
+      fontSize: theme.typography.pxToRem(18),
+      padding: theme.spacing(2),
+      width: '100%',
+    },
+  }),
+)
+
