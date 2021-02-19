aac5fb968ee6f1425c057a59b1cf53cc5c708970
tadashim <ma.tadashi@gmail.com>
2021-02-19 22:09:30 +0900

    Add components and the App function

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ import type {
 
 const drawerWidth = 300
 
-const useStyle = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
@@ -96,3 +96,373 @@ const useStyle = makeStyles((theme: Theme) =>
   }),
 )
 
+type NotesListProps = {
+  deleteDraft: () => void
+  openDraft: () => void
+  openNote: (docID: string) => void
+  state: State
+}
+
+function NotesList({
+  deleteDraft,
+  openDraft,
+  openNote,
+  state,
+}: NotesListProps) {
+  let draft
+  if (state.nav.type === 'draft') {
+    let icon
+    switch (state.draftStatus) {
+      case 'failed':
+        icon = <ErrorIcon />
+        break
+      case 'saving':
+        icon = <UploadIcon />
+        break
+      default:
+        icon = <EditIcon />
+    }
+    draft = (
+      <ListItem button onClick={() => openDraft()} selected>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary="Draft note" />
+        <ListItemSecondaryAction>
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            onClick={() => deleteDraft()}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    )
+  } else if (state.auth.status === 'done') {
+    draft = (
+      <ListItem>
+        <ListItemIcon>
+          <NoteAddIcon />
+        </ListItemIcon>
+        <ListItemText primary="New note" />
+      </ListItem>
+    )
+  } else {
+    draft = (
+      <ListItem>
+        <ListItemIcon>
+          <NoteAddIcon />
+        </ListItemIcon>
+        <ListItemText primary="Authenticate to create note" />
+      </ListItem>
+    )
+  }
+
+  const notes = Object.entries(state.notes).map(([docID, note]) => {
+    const isSelected = state.nav.type === 'note' && state.nav.docID === docID
+
+    let icon
+    switch (note.status) {
+      case 'loading failed':
+      case 'saving failed':
+        icon = <ErrorIcon />
+        break
+      case 'loading':
+        icon = <DownloadIcon />
+        break
+      case 'saving':
+        icon = <UploadIcon />
+        break
+      default:
+        icon = isSelected ? <EditIcon /> : <NoteIcon />
+    }
+
+    return (
+      <ListItem
+        button
+        key={docID}
+        onClick={() => openNote(docID)}
+        selected={isSelected}
+      >
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary={note.title} />
+      </ListItem>
+    )
+  })
+
+  return (
+    <>
+      <List>{draft}</List>
+      <Divider />
+      <List>{notes}</List>
+    </>
+  )
+}
+
+type AuthenticateProps = {
+  authenticate: (seed: Uint8Array) => void
+  state: AuthState
+}
+
+function AuthenticateScreen({ authenticate, state }: AuthenticateProps) {
+  const [seed, setSeed] = useState('')
+  const isLoading = state.status === 'loading'
+
+  return state.status === 'done' ? (
+    <Typography>Authenticated with ID {state.idx.id}</Typography>
+  ) : (
+    <>
+      <Typography>
+        You need to authenticate to load your existing notes and create new ones.
+      </Typography>
+      <div>
+        <TextField
+          autoFocus
+          disabled={isLoading}
+          fullWidth
+          id="seed"
+          label="Seed"
+          onChange={(event) => setSeed(event.target.value)}
+          placeholder="base16-encoded string of 32 bytes length"
+          type="text"
+          value={seed}
+        />
+      </div>
+      <Button
+        color="primary"
+        disabled={seed === '' || isLoading}
+        onClick={() => authenticate(fromString(seed, 'base16'))}
+        variant="contained"
+      >
+        Authenticate
+      </Button>
+      <Button
+        color="primary"
+        disabled={isLoading}
+        onClick={() => setSeed(toString(randomBytes(32), 'base16'))}
+      >
+        Generate random seed
+      </Button>
+    </>
+  )
+}
+
+type DraftScreenProps = {
+  save: (title: string, text: string) => void
+  status: DraftStatus
+}
+
+function DraftScreen({ save, status }: DraftScreenProps) {
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+  const textRef = useRef<HTMLTextAreaElement>(null)
+  const titleRef = useRef<HTMLInputElement>(null)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleSave = () => {
+    const text = textRef.current?.value
+    const title = titleRef.current?.value
+    if (text && title) {
+      save(title, text)
+    }
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Save note</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="title"
+            label="Note title"
+            inputRef={titleRef}
+            type="text"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSave} color="primary" variant="outlined">
+            Save note
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Paper elevation={5}>
+        <TextareaAutosize
+          className={classes.noteTextarea}
+          placeholder="Note contents..."
+          ref={textRef}
+          rowsMin={10}
+          rowsMax={20}
+        />
+      </Paper>
+      <Button
+        className={classes.noteSaveButton}
+        color="primary"
+        disabled={status === 'saving'}
+        onClick={handleOpen}
+        variant="contained"
+      >
+        Save
+      </Button>
+    </>
+  )
+}
+
+type NoteScreenProps = {
+  note: IndexLoadedNote | StoredNote
+  save: (doc: Doctype, text: string) => void
+}
+
+function NoteScreen({ note, save }: NoteScreenProps) {
+  const classes = useStyles()
+  const textRef = useRef<HTMLTextAreaElement>(null)
+
+  if (note.status === 'loading failed') {
+    return <Typography>Failed to load note!</Typography>
+  }
+
+  if (note.status === 'init' || note.status === 'loading') {
+    return <Typography>Loading note...</Typography>
+  }
+
+  const doc = (note as StoredNote).doc
+  return (
+    <>
+      <Paper elevation={5}>
+        <TextareaAutosize
+          className={classes.noteTextarea}
+          defaultValue={doc.content.text}
+          placeholder="Note contents..."
+          ref={textRef}
+          rowsMin={10}
+          rowsMax={20}
+        />
+      </Paper>
+      <Button
+        className={classes.noteSaveButton}
+        color="primary"
+        disabled={note.status === 'saving'}
+        onClick={() => save(doc, textRef.current?.value ?? '')}
+        variant="contained"
+      >
+        Save
+      </Button>
+    </>
+  )
+}
+
+export default function App() {
+  const app = useApp()
+  const classes = useStyles()
+  const theme = useTheme()
+  const [mobileOpen, setMobileOpen] = useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <NotesList
+        deleteDraft={app.deleteDraft}
+        openDraft={app.openDraft}
+        openNote={app.openNote}
+        state={app.state}
+      />
+    </div>
+  )
+
+  let screen
+  switch (app.state.nav.type) {
+    case 'draft':
+      screen = (
+        <DraftScreen save={app.saveDraft} status={app.state.draftStatus} />
+      )
+      break
+    case 'note':
+      screen = (
+        <NoteScreen
+          key={app.state.nav.docID}
+          note={app.state.notes[app.state.nav.docID]}
+          save={app.saveNote}
+        />
+      )
+      break
+    default:
+      screen = (
+        <AuthenticateScreen
+          authenticate={app.authenticate}
+          state={app.state.auth}
+        />
+      )
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} noWrap variant="h6">
+            IDX demo notes app
+          </Typography>
+          <Button color="inherit" href="<https://idx.xyz>" variant="outlined">
+            IDX
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="notes">
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{ paper: classes.drawerPaper }}
+            ModalProps={{ keepMounted: true }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{ paper: classes.drawerPaper }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {screen}
+      </main>
+    </div>
+  )
+}
